4e88af1cd9ca0e839553458b14cfa153dc8b6cb4
RonnieWillyamsRws <ronnie.willyams.rws@gmail.com>
2022-10-12 04:08:17 -0300

    Slider
    slider com javascript.

---
+++ scripts.js
@@ -0,0 +1,30 @@
+
+let lastIndex = 0;
+
+let images = document.querySelectorAll('.container img');
+
+images.forEach((item,index)=>{
+
+    document.querySelectorAll('.bullet-single')[index]
+    .addEventListener('click',()=>{
+
+         let = lastImage = document.querySelectorAll('.container img')[lastIndex];
+         let actualImage = document.querySelectorAll('.container img')[index];
+         
+         document.querySelectorAll('.bullet-single')[lastIndex]
+           
+         .classList.remove('active-bullet');
+         ;
+
+         document.querySelectorAll('.bullet-single')[index]
+            
+           .classList.add('active-bullet');
+
+         lastImage.style.opacity = 0;
+         actualImage.style.opacity = 1;
+
+         lastIndex = index;
+
+    })
+
+})
